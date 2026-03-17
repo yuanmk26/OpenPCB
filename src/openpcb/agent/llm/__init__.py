3e83b02b61d95912a20657b933f49cc0a1d26b4a
yuanmk26 <yuanmk25@163.com>
2026-03-17 20:46:52 +0800

    update

--- src/openpcb/agent/llm/__init__.py
+++
@@ -1,6 +0,0 @@
-"""LLM client abstractions."""
-
-from .factory import get_llm_client
-from .types import LLMError, LLMRequest, LLMResponse
-
-__all__ = ["get_llm_client", "LLMRequest", "LLMResponse", "LLMError"]
--- src/openpcb/io/__init__.py
+++
@@ -1,1 +0,0 @@
-"""I/O helpers for OpenPCB."""
